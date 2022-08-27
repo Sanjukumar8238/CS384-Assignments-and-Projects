4c69de6b1984516936696b8a32a2943adc6c46e4
Sanjukumar8238 <sanjuphogat8238@gmail.com>
2022-08-27 14:15:59 +0530

    Updated answer

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,1 +1,10 @@
-print("Hello World")
+def factorial( x):
+    ans=1;
+    for i in range (2,x+1):
+        ans=ans*i;
+    print(ans)
+
+    
+
+x=int(input("Enter the number whose factorial is to be found : "))
+factorial(x)
