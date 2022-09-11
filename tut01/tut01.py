a441f07bd95f2aae3fc7dba066d3d4440c06201a
Sanjukumar8238 <sanjuphogat8238@gmail.com>
2022-09-12 03:37:54 +0530

    Added the functionality of mod value for dividing the file in ranges

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -40,7 +40,6 @@ for i in df.index:
     octant.append(find_octant(df[r"U'=U - U Avg"][i],df[r"V'=V - V Avg"][i],df[r"W'=W - W Avg"][i]))
 df['Octant']=octant
 df.to_csv('output.csv')
-
 count=[0]*8
 for i in octant:
     if(i==1):
@@ -65,5 +64,37 @@ print('Overall Count\t',end="")
 for i in count:
     print(i,'\t',end="")
 print("")
+5
+x=int(input("Enter mod value : "))
+n=len(octant)
+
+count=[0]*8
+k=0
+for i in octant:
+    if(i==1):
+        count[0]=count[0]+1
+    elif(i==-1):
+        count[1]=count[1]+1
+    elif(i==2):
+        count[2]=count[2]+1
+    elif(i==-2):
+        count[3]=count[3]+1
+    elif(i==3):
+        count[4]=count[4]+1
+    elif(i==-3):
+        count[5]=count[5]+1
+    elif(i==4):
+        count[6]=count[6]+1
+    elif(i==-4):
+        count[7]=count[7]+1
+    k=k+1
+    if(k%x==1):
+        print(k,'-',end="")
+    elif(k%x==0 or k==n):
+        print(k,'\t',end="")
+        for j in count:
+            print(j,'\t',end="")
+        print("")
+        count=[0]*8
 
 
