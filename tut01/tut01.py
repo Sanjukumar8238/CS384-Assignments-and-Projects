c8bef0253ca81ff7ab40e4a1628422db8b8e9237
Sanjukumar8238 <sanjuphogat8238@gmail.com>
2022-09-11 17:09:27 +0530

    Data pre-processing is done
    Subtracted mean from the original velocities and created columns of these values.

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,10 +1,25 @@
-def factorial( x):
-    ans=1;
-    for i in range (2,x+1):
-        ans=ans*i;
-    print(ans)
+import csv
+import os
+import pandas as pd
+os.chdir(r"C:\Users\Sanju Phogat\Documents\GitHub\2001EE62_2022\tut01")
 
-    
+df=pd.read_csv('octant_input.csv')
+u_avg=df['U'].mean()
+v_avg=df['V'].mean()
+w_avg=df['W'].mean()
+df['U Avg']=u_avg
+df['V Avg']=v_avg
+df['W Avg']=w_avg
 
-x=int(input("Enter the number whose factorial is to be found : "))
-factorial(x)
+df[r"U'=U-U Avg"]=df['U']-df['U Avg']
+df[r"V'=V-V Avg"]=df['V']-df['V Avg']
+df[r"W'=W-W Avg"]=df['W']-df['W Avg']
+
+df.to_csv('output.csv')
+#def octact_identification(mod=5000):
+###Code
+
+
+#mod=5000
+#octact_identification(mod)
+
