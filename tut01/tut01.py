961533dc257d939022e55924f6f6e84b5a781c0d
Sanjukumar8238 <sanjuphogat8238@gmail.com>
2022-09-12 03:41:27 +0530

    Removed local machine configuration for running code

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,9 +1,5 @@
 import csv
-from email import header
-import os
-from xml.etree.ElementTree import tostring
 import pandas as pd
-os.chdir(r"C:\Users\Sanju Phogat\Documents\GitHub\2001EE62_2022\tut01")
 
 def find_octant(a,b,c):
     if(a>0 and b>0 and c>0):
@@ -39,7 +35,6 @@ octant=[]
 for i in df.index:
     octant.append(find_octant(df[r"U'=U - U Avg"][i],df[r"V'=V - V Avg"][i],df[r"W'=W - W Avg"][i]))
 df['Octant']=octant
-df.to_csv('output.csv')
 count=[0]*8
 for i in octant:
     if(i==1):
