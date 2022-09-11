277022cab1b8f1bf0eeb9b17c9fee8abb477e7b0
Sanjukumar8238 <sanjuphogat8238@gmail.com>
2022-09-11 17:26:13 +0530

    Added Octant column to specify the octant

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -3,6 +3,25 @@ import os
 import pandas as pd
 os.chdir(r"C:\Users\Sanju Phogat\Documents\GitHub\2001EE62_2022\tut01")
 
+def find_octant(a,b,c):
+    if(a>0 and b>0 and c>0):
+        return 1
+    elif(a>0 and b>0 and c<0):
+        return -1
+    elif(a<0 and b>0 and c>0):
+        return 2
+    elif(a<0 and b>0 and c<0):
+        return -2
+    elif(a<0 and b<0 and c>0):
+        return 3
+    elif(a<0 and b<0 and c<0):
+        return -3
+    elif(a>0 and b<0 and c>0):
+        return 4
+    elif(a>0 and b<0 and c<0):
+        return -4
+
+
 df=pd.read_csv('octant_input.csv')
 u_avg=df['U'].mean()
 v_avg=df['V'].mean()
@@ -11,9 +30,14 @@ df['U Avg']=u_avg
 df['V Avg']=v_avg
 df['W Avg']=w_avg
 
-df[r"U'=U-U Avg"]=df['U']-df['U Avg']
-df[r"V'=V-V Avg"]=df['V']-df['V Avg']
-df[r"W'=W-W Avg"]=df['W']-df['W Avg']
+df[r"U'=U - U Avg"]=df['U']-df['U Avg']
+df[r"V'=V - V Avg"]=df['V']-df['V Avg']
+df[r"W'=W - W Avg"]=df['W']-df['W Avg']
+octant=[]
+for i in df.index:
+    octant.append(find_octant(df[r"U'=U - U Avg"][i],df[r"V'=V - V Avg"][i],df[r"W'=W - W Avg"][i]))
+df['Octant']=octant
+    
 
 df.to_csv('output.csv')
 #def octact_identification(mod=5000):
