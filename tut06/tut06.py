d51d994db283a8f64c149006a13986e57411b2b7
Sanjukumar8238 <sanjuphogat8238@gmail.com>
2022-10-15 22:24:51 +0530

    Option added for the user to email the ”attendance report consolidated.csv”

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -2,6 +2,11 @@ from ctypes import sizeof
 import pandas as pd
 import csv
 import os
+import email, smtplib, ssl
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 from datetime import datetime
 start_time = datetime.now()
@@ -112,6 +117,57 @@ def attendance_report():
             for key,value in count_of_roll.items():
                 if(value>1):
                     writer.writerow([date_of_duplicate,key,name[key],value])
+    
+    def send_email():
+        try:
+            subject = "Consolidated Attendace Report"
+            body = "The report is attached with this mail."
+            sender_email = input("Enter sender email : ")
+            receiver_email = "cs3842022@gmail.com"
+            password = input("Type your password and press enter:")
+
+            # Create a multipart message and set headers
+            message = MIMEMultipart()
+            message["From"] = sender_email
+            message["To"] = receiver_email
+            message["Subject"] = subject
+            message["Bcc"] = receiver_email  # Recommended for mass emails
+
+            # Add body to email
+            message.attach(MIMEText(body, "plain"))
+
+            filename = "output/attendance_report_consolidated.csv"  # In same directory as script
+
+            # Open csv file in binary mode
+            with open(filename, "rb") as attachment:
+                # Add file as application/octet-stream
+                # Email client can usually download this automatically as attachment
+                part = MIMEBase("application", "octet-stream")
+                part.set_payload(attachment.read())
+
+            # Encode file in ASCII characters to send by email    
+            encoders.encode_base64(part)
+
+            # Add header as key/value pair to attachment part
+            part.add_header(
+                "Content-Disposition",
+                f"attachment; filename= {filename}",
+            )
+            # Add attachment to message and convert message to string
+            message.attach(part)
+            text = message.as_string()
+
+            # Log in to server using secure context and send email
+            context = ssl.create_default_context()
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+                server.login(sender_email, password)
+                server.sendmail(sender_email, receiver_email, text)
+        except:
+            print("Error in sending the file.")
+
+    ans=input("Do you want to email to cs3842022@gmail.com (Y/N) : ")
+    if(ans=='Y'):
+        send_email()
 
 from platform import python_version
 ver = python_version()
