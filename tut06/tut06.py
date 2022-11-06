02dfb56474975058f6b21b8fa9d2294f0a0d053c
Sanjukumar8238 <sanjuphogat8238@gmail.com>
2022-11-06 11:23:32 +0530

    Found the dates that found on Monday and Thursday

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -27,7 +27,24 @@ def attendance_report():                                            # Function t
         name[roll_no]=df['Name'][i]                                 # Mapping roll numbers to names of students
     n=len(rolls)                                                    # Finding number of roll numbers
     
-            
+    del df
+    df1=pd.read_csv('input_attendance.csv')                         # Reading input_attendace file 
+    df = df1[df1['Attendance'].notnull()]                           # Removing the rows which have name as empty
+    x=len(df)
+    format="%d-%m-%Y"                                       # Formatting the date
+    start=datetime.strptime(df['Timestamp'][0].split()[0],format)                 # Converting date string to date object supported by python
+    end=datetime.strptime(df['Timestamp'][x-1].split()[0],format)
+  
+    datesRange = pd.date_range(start, end, freq='D')
+    lectures=[]
+    for i in range(len(datesRange)):
+        if(datesRange[i].weekday()==0 or datesRange[i].weekday()==3):
+            lectures.append(datesRange[i].strftime("%d-%m-%Y"))
+    
+    for i in range(len(lectures)):
+        date_mapping[lectures[i]]=i
+
+             
         
 from platform import python_version
 ver = python_version()
