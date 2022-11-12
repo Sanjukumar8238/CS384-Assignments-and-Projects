b27a9816f22b49a2da2f5be8257cce9a8032e119
Sanjukumar8238 <sanjuphogat8238@gmail.com>
2022-11-12 11:29:17 +0530

    Comments and try-except blocks are added.

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -1,37 +1,34 @@
-
-from datetime import datetime
+from datetime import datetime							# Importing required header files
 start_time = datetime.now()
 import textwrap as tr
 
 #Help
 
-
-
-def scorecard():
+def scorecard():										# Function to generate scorecard
 	
-	players={}
-	power_play1=0
-	power_play2=0
-	bat1=[]
-	bowl1=[]
-	bat2=[]
-	bowl2=[]
-	total1=[0]*3
-	total2=[0]*3
-	table1=[]
-	table2=[]
-	table3=[]
-	table4=[]
-	extra1=[0]*6
-	extra2=[0]*6
-	fall_of_wickets1=[]
-	fall_of_wickets2=[]
-	did_not_bat1=[]
-	did_not_bat2=[]
+	players={}											# Dictionary to map players according to their first and last names
+	power_play1=0										# Variable for Power-play runs -> team1
+	power_play2=0										# Variable for Power-play runs -> team2
+	bat1=[]												# List of batsman in team1
+	bowl1=[]											# List of bowler in team1
+	bat2=[]												# List of batsman in team2
+	bowl2=[]											# List of bowler in team2
+	total1=[0]*3										# Storing total values-> Runs, Wickets, Overs - team1
+	total2=[0]*3										# Storing total values-> Runs, Wickets, Overs - team2
+	table1=[]											# Batting stats of team1
+	table2=[]											# Bowling stats of team2
+	table3=[]											# Batting stats of team2
+	table4=[]											# Bowling stats of team1
+	extra1=[0]*6										# Extra runs - [sum of extra runs,b,lb,w,nb,p] - team1
+	extra2=[0]*6										# Extra runs - [sum of extra runs,b,lb,w,nb,p] - team2
+	fall_of_wickets1=[]									# List for containing fall of wickets - team1
+	fall_of_wickets2=[]									# List for containing fall of wickets - team2
+	did_not_bat1=[]										# Players that didn't bat in team1
+	did_not_bat2=[]										# Players that didn't bat in team1
 
-	def find_indexof_bowler_batsman(bowler,batsman,x):
-		temp=[]
-		if(x==1):
+	def find_indexof_bowler_batsman(bowler,batsman,x):	# Function for finding index of bowler and batsman
+		temp=[]											# []=[index of batsman, index of bowler]- for storing in tables
+		if(x==1):										# For 1st innings
 			for i in range(len(bat1)):
 				if(bat1[i]==batsman):
 					temp.append(i)
@@ -40,7 +37,7 @@ def scorecard():
 				if(bowl1[i]==bowler):
 					temp.append(i)
 					break
-		else:
+		else:											# For 2nd innings
 			for i in range(len(bat2)):
 				if(bat2[i]==batsman):
 					temp.append(i)
@@ -50,67 +47,67 @@ def scorecard():
 					temp.append(i)
 					break
 		return temp
-	def no_run(t1,t2,bowler,batsman,m):
+	def no_run(t1,t2,bowler,batsman,m):					# If no runs is scored in single ball
 		x=find_indexof_bowler_batsman(bowler,batsman,m)
 		t1[x[0]][2]+=1
 		t2[x[1]][0]+=1
-	def runs(t1,t2,bowler,batsman,y,m):
+	def runs(t1,t2,bowler,batsman,y,m):					# If y runs is scored in single ball
 		x=find_indexof_bowler_batsman(bowler,batsman,m)
 		t1[x[0]][2]+=1
 		t2[x[1]][0]+=1
 		t1[x[0]][1]+=y
 		t2[x[1]][2]+=y
 		
-	def wide(t1,t2,bowler,batsman,m):
+	def wide(t1,t2,bowler,batsman,m):					# If ball is wide
 		x=find_indexof_bowler_batsman(bowler,batsman,m)
 		t2[x[1]][5]+=1
 		t2[x[1]][2]+=1
-	def wides(t1,t2,bowler,batsman,z,m):
+	def wides(t1,t2,bowler,batsman,z,m):				# If z balls are wide
 		x=find_indexof_bowler_batsman(bowler,batsman,m)
 		t2[x[1]][5]+=z
 		t2[x[1]][2]+=z
 		
-	def byes(t1,t2,bowler,batsman,z,m):
+	def byes(t1,t2,bowler,batsman,z,m):					# If 1 run is scored in byes
 		x=find_indexof_bowler_batsman(bowler,batsman,m)
 		t1[x[0]][2]+=1
 		t2[x[1]][0]+=1
 		
-	def caught_out(t1,t2,bowler,batsman,catcher,m):
+	def caught_out(t1,t2,bowler,batsman,catcher,m):		# If batsman is caught out
 		x=find_indexof_bowler_batsman(bowler,batsman,m)
 		t1[x[0]][2]+=1
 		t2[x[1]][0]+=1
 		t1[x[0]][0]='c '+catcher+' b '+bowler
 		t2[x[1]][3]+=1
 		
-	def lbw(t1,t2,bowler,batsman,m):
+	def lbw(t1,t2,bowler,batsman,m):					# If batsman is out by lbw
 		x=find_indexof_bowler_batsman(bowler,batsman,m)
 		t1[x[0]][2]+=1
 		t2[x[1]][0]+=1
 		t2[x[1]][3]+=1
 		t1[x[0]][0]='lbw b '+bowler
 		
-	def bowled(t1,t2,bowler,batsman,m):
+	def bowled(t1,t2,bowler,batsman,m):					# If batsman is bowled
 		x=find_indexof_bowler_batsman(bowler,batsman,m)
 		t1[x[0]][2]+=1
 		t2[x[1]][0]+=1
 		t2[x[1]][3]+=1
 		t1[x[0]][0]='b '+bowler
 		
-	def bowls_to_overs(t):
+	def bowls_to_overs(t):								# Function to converting balls to overs
 		for i in t:
 			res=str(i[0]//6)
 			if(i[0]%6!=0):
 				res+='.'+str(i[0]%6)
 			i[0]=res
-	def eco_calc(t):
+	def eco_calc(t):									# Calculating economy of a bowler
 		for i in t:
 			temp=i[2]*6/i[0]
 			i[6]='{0:.1f}'.format(temp)+'0'
-	def SR_calc(t):
+	def SR_calc(t):										# Calculating strike rate of a batsman
 		for i in t:
 			temp=i[1]*100/i[2]
 			i[5]='{0:.2f}'.format(temp)
-	def total_score_calc(t1,t2,total1):
+	def total_score_calc(t1,t2,total1):					# Converting balls in total variable to overs
 		for i in t1:
 			total1[2]+=i[2]
 		res=str(total1[2]//6)
@@ -118,10 +115,13 @@ def scorecard():
 			res+='.'+str(total1[2]%6)
 		total1[2]=res
 
+	try:
+		file1=open('teams.txt','r')							# Opening teams.txt file 
+	except:
+		print('Error in opening teams.txt file')
+	Lines=file1.readlines()								
 
-	file1=open('teams.txt','r')
-	Lines=file1.readlines()
-	pak_team=Lines[0].strip().split(':')[1].split(',')
+	pak_team=Lines[0].strip().split(':')[1].split(',')	# Storing names of team1 players and mapping according to their first and last names
 	for i in range(len(pak_team)):
 		pak_team[i]=pak_team[i].strip().split('(')[0]
 		x=pak_team[i].split(' ')
@@ -129,7 +129,7 @@ def scorecard():
 		players[x[1]]=pak_team[i]
 		players[pak_team[i]]=pak_team[i]
 		
-	ind_team=Lines[2].strip().split(':')[1].split(',')
+	ind_team=Lines[2].strip().split(':')[1].split(',')	# Storing names of team2 players and mapping according to their first and last names
 	for i in range(len(ind_team)):
 		ind_team[i]=ind_team[i].strip().split('(')[0]
 		x=ind_team[i].split(' ')
@@ -138,13 +138,11 @@ def scorecard():
 		players[ind_team[i]]=ind_team[i]
 	file1.close()
 
-	file1=open('pak_inns1.txt','r')
+	try:
+		file1=open('pak_inns1.txt','r')						# Pakistan's innings file is opened
+	except:
+		print('Error in opening pak_inns1.txt')
 	Lines=file1.readlines()
-	
-	
-	
-
-
 	for line in Lines:
 		if(line=='\n'):
 			continue
@@ -152,14 +150,14 @@ def scorecard():
 		x=line[0].strip().split(' ')
 		y=line[1].strip()
 		
-		if(x[2]=='to'):
+		if(x[2]=='to'):									# Figuring out bowler and batsman
 			batsman=players[x[3]]
 			bowler=players[x[1]]
 		elif(x[3]=='to'):
 			batsman=players[x[4]]
 			bowler=players[x[1]]
-	
-		if(batsman not in bat1):
+
+		if(batsman not in bat1):						# If batsman or bowler is not in the bat1 or bowl1, then add a new row to table1 or table2 respectively
 			bat1.append(batsman)
 			temp=['not out',0,0,0,0,0]
 			table1.append(temp)
@@ -168,7 +166,8 @@ def scorecard():
 			temp=[0,0,0,0,0,0,0]
 			table2.append(temp)
 
-		if(y=='no run'):
+		# Functions are called according to the event happened in each ball
+		if(y=='no run'):								
 			no_run(table1,table2,bowler,batsman,1)
 		elif(y=='byes'):
 			z=int(line[2].strip().split(' ')[0])
@@ -219,22 +218,25 @@ def scorecard():
 			bowled(table1,table2,bowler,batsman,1)
 			total1[1]+=1
 
-		if(y.split(' ')[0]=='out'):
+		if(y.split(' ')[0]=='out'):							# Storing fall of wickets
 			fall_of_wickets1.append(str(total1[0])+'-'+str(total1[1])+' ('+batsman+', '+x[0]+')')
-		if(x[0]=='5.6'):
+		if(x[0]=='5.6'):									# Powerplay over
 			power_play1=total1[0]
-	for i in pak_team:
+	for i in pak_team:										# Players that did not bat
 		if(i not in bat1):
 			did_not_bat1.append(i)
 
-	eco_calc(table2)
+	eco_calc(table2)										# Remaining functions are called
 	bowls_to_overs(table2)
 	SR_calc(table1)
 	total_score_calc(table1,table2,total1=total1)
 	
 	
 	file1.close()
-	file1=open('india_inns2.txt','r')
+	try:
+		file1=open('india_inns2.txt','r')						# India's innings file is opened
+	except:
+		print('Error in opening india_inns2.txt')
 	Lines=file1.readlines()
 	for line in Lines:
 		if(line=='\n'):
@@ -243,14 +245,14 @@ def scorecard():
 		x=line[0].strip().split(' ')
 		y=line[1].strip()
 		
-		if(x[2]=='to'):
+		if(x[2]=='to'):										# Figuring out bowler and batsman
 			batsman=players[x[3]]
 			bowler=players[x[1]]
 		elif(x[3]=='to'):
 			batsman=players[x[4]]
 			bowler=players[x[1]]
 	
-		if(batsman not in bat2):
+		if(batsman not in bat2):							# If batsman or bowler is not in the bat1 or bowl1, then add a new row to table3 or table4 respectively
 			bat2.append(batsman)
 			temp=['not out',0,0,0,0,0]
 			table3.append(temp)
@@ -259,6 +261,7 @@ def scorecard():
 			temp=[0,0,0,0,0,0,0]
 			table4.append(temp)
 
+		# Functions are called according to the event happened in each ball
 		if(y=='no run'):
 			no_run(table3,table4,bowler,batsman,2)
 		elif(y=='byes'):
@@ -329,21 +332,27 @@ def scorecard():
 		else:
 			print(line)
 
-		if(y.split(' ')[0]=='out'):
+		if(y.split(' ')[0]=='out'):							# Storing fall of wickets
 			fall_of_wickets2.append(str(total2[0])+'-'+str(total2[1])+' ('+batsman+', '+x[0]+')')
-		if(line[0].strip().split(' ')[0]=='5.6'):
+		if(line[0].strip().split(' ')[0]=='5.6'):			# Powerplay over
 			power_play2=total2[0]
-	for i in ind_team:
+	for i in ind_team:										# Players that did not bat
 		if(i not in bat2):
 			did_not_bat2.append(i)
-	eco_calc(table4)
+	
+	eco_calc(table4)										# Remaining functions are called
 	bowls_to_overs(table4)
 	SR_calc(table3)
 	total_score_calc(table3,table4,total1=total2)
 	
 
 	file1.close()
-	file=open('Scorecard.txt','w')
+	try:
+		file=open('Scorecard.txt','w')							# Opening Scorecard file for writing to it
+	except:
+		print('Error in opening Scorecard.txt file')
+	# Pakistan's Scorecard written
+
 	file.write("{: >0} {: >89}".format(*['Pakistan Innings',str(total1[0])+'-'+str(total1[1])+' ('+total1[2]+' Ov)\n']))
 	file.write('\n')
 	file.write("{: <20} {: ^45}{: ^8}{: ^8}{: ^8}{: ^8}{: >8}".format(*['Batter','','R','B','4s','6s','SR\n']))
@@ -372,6 +381,8 @@ def scorecard():
 	file.write('\n')
 	file.write('\n')
 
+
+	# India's Scorecard written
 
 	file.write("{: >0} {: >92}".format(*['India Innings',str(total2[0])+'-'+str(total2[1])+' ('+total2[2]+' Ov)\n']))
 	file.write('\n')
